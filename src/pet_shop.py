d6bb52cd5104f6dd06d9437e0b7223bf8bfb1145
jakubnecessary <cabbage1991@gmail.com>
2021-06-18 15:18:14 +0100

    Function remove cash

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,12 +1,21 @@
 # WRITE YOUR FUNCTIONS HERE
 
-# 1 Test pet_shop name
+# 1 Function pet_shop name 22
 
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
-# 2 Test total cash
+# 2 Function total cash 21
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+#   Function add cash  20
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
+    
+
+
+
+
 
