51d893237e8ab3025287a35c02933918762fb166
jakubnecessary <cabbage1991@gmail.com>
2021-06-18 11:55:13 +0100

    Function Get total cash

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,12 @@
 # WRITE YOUR FUNCTIONS HERE
+
+# 1 Test pet_shop name
+
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+# 2 Test total cash
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
