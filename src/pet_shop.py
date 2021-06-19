9c3f95d8cca159d79d3159756fcd511df439f03b
jakubnecessary <cabbage1991@gmail.com>
2021-06-19 15:59:47 +0100

    Working on for loop to get british shorthair by breed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,40 +1,52 @@
 # WRITE YOUR FUNCTIONS HERE
 
-# 1 Function pet_shop name 22
+# 1 Function pet_shop name      22
 
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
-# 2 Function total cash 21
+# 2 Function total cash      21
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
-#   Function add cash  20 += adds equal
+#   Function add cash  += adds equal    20
 
 def add_or_remove_cash(pet_shop, cash):
     pet_shop["admin"]["total_cash"] += cash
 
-# # Function remove cash 19
+# # Function remove cash 
+# doesnt return just changes     19
 
 def add_or_remove_cash(pet_shop, cash_remove):
     pet_shop["admin"]["total_cash"] += cash_remove
 
-# Function get pets sold 18
+# Function get pets sold   18
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
-# Function to increse pets sold by 2 += adds equal
+# Function to increse pets
+#  sold by 2 += adds equal  17
 
 def increase_pets_sold(pet_sold, add_pet):
     pet_sold["admin"]["pets_sold"] += add_pet
 
 # function to get stock count = 6 use len()
-#  to return number of characters in the string
+#  to return number of 
+# characters in the string    16
 
 def get_stock_count(pets_count):
     return len(pets_count["pets"])
+
+# function to get  1 pet by breed British Shorthair
+#  make a loop "pets" "breed"  15
+
+def get_pets_by_breed(pet, pets):
+    for pet in pets:
+        if pet["pets"]["breed"] == "British Shorthair":
+            return len(pet["pets"]["breed"])
+
 
 
 
